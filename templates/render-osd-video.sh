27eebe5d3b1c99dac768b7d8ce8c8c0f7df4e0fe
Anthony Scopatz <scopatz@gmail.com>
2019-11-26 10:30:38 -0500

    kuisever

--- templates/render-osd-video.sh
+++ templates/render-osd-video.sh
@@ -5,5 +5,5 @@ MELT="{{ melt }}"
 
 SOURCE_0="file://{{ mlt_file }}"
 TARGET_0="file://{{ webm_file }}"
-PARAMETERS_0="-pid:20377 in=0 out=85096 $MELT atsc_1080p_2997 avformat - $SOURCE_0 $TARGET_0 f=webm vcodec=libvpx acodec=libvorbis crf=15 vb=0 quality=good aq=7 max-intra-rate=1000 cpu-used=4 threads=1 real_time=-1"
+PARAMETERS_0="- in=0 out=85096 $MELT atsc_1080p_2997 avformat - $SOURCE_0 $TARGET_0 f=webm vcodec=libvpx acodec=libvorbis crf=15 vb=0 quality=good aq=7 max-intra-rate=1000 cpu-used=4 threads=1 real_time=-1"
 $RENDERER $PARAMETERS_0
